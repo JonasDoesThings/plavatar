3673cb44e63668fc9ff451662e4ede7ff39a21ae
jonasdoesthings <jonas@jonasdoesthings.com>
2021-06-08 16:04:03 +0200

    fix: correctly register RemoveTrailingSlash middleware

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -2,18 +2,13 @@ package api
 
 func (server *Server) routes() {
 	server.echoRouter.GET("/pixel/:size/:name", server.HandleGetPixelAvatar())
-	server.echoRouter.GET("/pixel/:size/", server.HandleGetPixelAvatar())
 	server.echoRouter.GET("/pixel/:size", server.HandleGetPixelAvatar())
 	server.echoRouter.GET("/solid/:size/:name", server.HandleGetSolidAvatar())
-	server.echoRouter.GET("/solid/:size/", server.HandleGetSolidAvatar())
 	server.echoRouter.GET("/solid/:size", server.HandleGetSolidAvatar())
 	server.echoRouter.GET("/gradient/:size/:name", server.HandleGetGradientAvatar())
-	server.echoRouter.GET("/gradient/:size/", server.HandleGetGradientAvatar())
 	server.echoRouter.GET("/gradient/:size", server.HandleGetGradientAvatar())
 	server.echoRouter.GET("/marble/:size/:name", server.HandleGetMarbleAvatar())
-	server.echoRouter.GET("/marble/:size/", server.HandleGetMarbleAvatar())
 	server.echoRouter.GET("/marble/:size", server.HandleGetMarbleAvatar())
 	server.echoRouter.GET("/laughing/:size/:name", server.HandleGetLaughingAvatar())
-	server.echoRouter.GET("/laughing/:size/", server.HandleGetLaughingAvatar())
 	server.echoRouter.GET("/laughing/:size", server.HandleGetLaughingAvatar())
 }
