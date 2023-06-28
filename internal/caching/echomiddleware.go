ca4efab2b99dd11651cacfc9866da9cee495d597
JonasDoesThings <jonas@jonasdoesthings.com>
2023-06-28 18:50:38 +0200

    feat: cache image/svg content type in middleware, not only image/png

--- internal/caching/echomiddleware.go
+++ internal/caching/echomiddleware.go
@@ -46,7 +46,8 @@ func CacheMiddleware(avatarCache *cache.Cache) echo.MiddlewareFunc {
 			}
 
 			// Check if the response is valid, AND if the response has a provided seed (:name). Don't cache random generated ones
-			if writer.statusCode == http.StatusOK && context.Response().Header().Get("Content-Type") == "image/png" && strings.Contains(context.Path(), ":name") {
+			contentType := context.Response().Header().Get("Content-Type")
+			if writer.statusCode == http.StatusOK && (contentType == "image/png" || contentType == "image/svg+xml") && strings.Contains(context.Path(), ":name") {
 				avatarCache.SetDefault(context.Request().RequestURI, resBody.Bytes())
 			}
 
