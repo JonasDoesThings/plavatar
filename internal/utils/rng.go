3df557091dc3970327fad273318acb08471f5a52
JonasDoesThings <jonas@jonasdoesthings.com>
2023-07-01 11:17:40 +0200

    refactor: move rng utils to one file

--- internal/utils/rng.go
+++ internal/utils/rng.go
@@ -1,8 +1,13 @@
 package utils
 
 import (
+	"fmt"
 	"math/rand"
 )
+
+func GetRandomColorHex(rng *rand.Rand) string {
+	return fmt.Sprintf("#%02X%02X%02X", rng.Intn(255), rng.Intn(255), rng.Intn(255))
+}
 
 func RandomRangeInt(rng *rand.Rand, min, max int) int {
 	return min + rng.Intn(max-min)
